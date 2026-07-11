c00761d9a7fd0e74be5c4148993abb7bad07b469
eliasjuk <eliasp.juk@hotmail.com>
2026-07-11 15:39:49 -0300

    feat: app

---
+++ app/lib/app.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import 'core/theme/app_theme.dart';
+import 'core/theme/theme_controller.dart';
+import 'pages/shell/app_shell.dart';
+
+class DoceCiaApp extends StatefulWidget {
+  const DoceCiaApp({super.key});
+
+  @override
+  State<DoceCiaApp> createState() => _DoceCiaAppState();
+}
+
+class _DoceCiaAppState extends State<DoceCiaApp> {
+  final ThemeController _themeController = ThemeController();
+
+  @override
+  void dispose() {
+    _themeController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _themeController,
+      builder: (context, _) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Doce Cia',
+          theme: AppTheme.light,
+          darkTheme: AppTheme.dark,
+          themeMode: _themeController.themeMode,
+          home: AppShell(themeController: _themeController),
+        );
+      },
+    );
+  }
+}
